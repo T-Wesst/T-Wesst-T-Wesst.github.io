d4530562f3ccf87fa7549408195cb6b928a770bb
T-Wesst <code.daddy17@gmail.com>
2018-12-08 22:03:47 -0800

    remove unneeded Project component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
-import Project from "./components/Project";
 import Projects from "./components/Projects";
 
 class App extends Component {
