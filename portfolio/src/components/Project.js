9818b66ed1bcfd14c874bd5594ea8f11c301789b
T-Wesst <code.daddy17@gmail.com>
2018-12-11 23:01:04 -0800

    change to stateless functional component

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,40 +1,38 @@
-import React, { Component } from "react";
-class Project extends Component {
-  render() {
-    const { imgSrc, desc, url, srcCode, title, alt } = this.props.project;
-    return (
-      <div className="card box-shadow">
-        <div className="item">
-          <img src={imgSrc} alt={alt} className="card-img-top" />
-          <div className="card-body">
-            <h4 className="text-center">{title}</h4>
-            <p className="card-body">{desc}</p>
-            <small>
-              <i className="fas fa-external-link-alt" />
-              <a
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="card-link"
-              >
-                Preview
-              </a>
-              <br />
-              <i className="fas fa-code" />
-              <a
-                href={srcCode}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="card-link"
-              >
-                Source
-              </a>
-            </small>
-          </div>
+import React from "react";
+const Project = props => {
+  const { imgSrc, desc, url, srcCode, title, alt } = props.project;
+  return (
+    <div className="card box-shadow">
+      <div className="item">
+        <img src={imgSrc} alt={alt} className="card-img-top" />
+        <div className="card-body">
+          <h4 className="text-center">{title}</h4>
+          <p className="card-body">{desc}</p>
+          <small>
+            <i className="fas fa-external-link-alt" />
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="card-link"
+            >
+              Preview
+            </a>
+            <br />
+            <i className="fas fa-code" />
+            <a
+              href={srcCode}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="card-link"
+            >
+              Source
+            </a>
+          </small>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Project;
